c46df521d67fd43092c5af8f8b6587b5d5eb11e6
Nicolas Hahn <nicolas@stonespring.org>
2020-09-26 14:26:16 -0700

    rm clap as dep, add armv7 compilation

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,47 @@
-use clap::{App, Arg};
 use nix::{
     sys::signal::{kill, SIGKILL},
     unistd::{getpid, Pid},
 };
-use std::{
-    collections::BTreeSet,
-    fs,
-    io::{self, Read},
-};
+use std::{collections::BTreeSet, env, error::Error, fs, io::Read};
 
-fn main() -> io::Result<()> {
-    let matches = App::new("killall")
-        .arg(
-            Arg::with_name("filenames")
-                .help("killall processes in the same group as the filename for each given")
-                .multiple(true)
-                .required(true),
-        )
-        .get_matches();
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut filenames = env::args().collect::<Vec<_>>();
+    filenames.remove(0);
+    if filenames.is_empty() {
+        return Err("you must pass at least one filename".into());
+    }
 
-    let filenames = matches.values_of("filenames").unwrap().collect::<Vec<_>>();
     let proc = "/proc/";
     let mut pgroups = BTreeSet::new();
 
-    for full_path in fs::read_dir(proc)?
-        .map(|res| res.map(|e| e.path()))
-        .collect::<io::Result<Vec<_>>>()?
-    {
-        full_path
-            .into_os_string()
-            .into_string()
-            .map(|fp| {
-                let rel_path = fp.split("/").collect::<Vec<_>>()[2];
-                if rel_path.chars().all(char::is_numeric) {
-                    let get_file_contents = |filename: &str| {
-                        let mut path = fp.to_owned();
-                        path.push_str(filename);
-                        let mut file = fs::File::open(path).unwrap();
-                        let mut contents = String::new();
-                        file.read_to_string(&mut contents).unwrap();
-                        contents
-                    };
+    for full_path in fs::read_dir(proc)?.map(|res| res.map(|e| e.path())) {
+        let fp_str = full_path?.into_os_string().into_string().unwrap();
 
-                    let stat = get_file_contents("/stat");
-                    let cmdline = get_file_contents("/cmdline");
+        let rel_path = fp_str.split("/").collect::<Vec<_>>()[2];
+        if rel_path.chars().all(char::is_numeric) {
+            let get_pid_file_contents = |filename: &str| -> Result<String, Box<dyn Error>> {
+                let mut path = fp_str.clone();
+                path.push_str(filename);
+                let mut contents = String::new();
+                fs::File::open(path)?.read_to_string(&mut contents)?;
 
-                    let neg_pgroup = Pid::from_raw(
-                        -stat.split(" ").collect::<Vec<_>>()[4]
-                            .parse::<i32>()
-                            .unwrap(),
-                    );
+                Ok(contents)
+            };
 
-                    for filename in &filenames {
-                        if cmdline.contains(filename) {
-                            let pid = Pid::from_raw(rel_path.to_string().parse::<i32>().unwrap());
-                            if getpid() != pid {
-                                pgroups.insert(neg_pgroup);
-                            }
-                        }
+            let stat = get_pid_file_contents("/stat")?;
+            let cmdline = get_pid_file_contents("/cmdline")?;
+
+            let neg_pgroup = Pid::from_raw(-stat.split(" ").collect::<Vec<_>>()[4].parse::<i32>()?);
+
+            for filename in &filenames {
+                if cmdline.contains(filename) {
+                    let pid = Pid::from_raw(rel_path.to_string().parse::<i32>()?);
+                    if getpid() != pid {
+                        pgroups.insert(neg_pgroup);
                     }
                 }
-            })
-            .unwrap();
+            }
+        }
     }
 
     for pgroup in pgroups {
